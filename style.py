23d960a0289182f10f77d5b5472686bedd623694
Atharv Joshi <joshiatharv337110@gmail.com>
2022-07-13 16:10:56 +0530

    first Commit

---
+++ style.py
@@ -0,0 +1,112 @@
+
+
+layout_style = {
+    'height': '',
+    'width': 'auto',
+    # 'backgroundColor': 'RGB(168,247,226)'
+    'background': 'RGB(15,40,48)',
+    'fontColor' : 'black'
+}
+uploader_style = {
+    'width': '100%',
+    'height': '90px',
+    'fontSize': '24px',
+    'borderWidth': '1px',
+    'borderStyle': 'dashed',
+    'borderRadius': '15px',
+    'textAlign': 'center',
+    'lineHeight': '90px',
+    'marginLeft': '0%',
+    'marginRight': 'auto',
+    'marginTop': '50px',
+    'color' : 'RGB(47,70,173)',
+    'borderColor': 'white'
+}
+
+h1_style = {
+    'color': 'RGB(85,99,85)',
+    'textAlign': 'center',
+    'fontStyle': 'italic'
+}
+
+sample_chart_style = {
+    'display':'flex',
+    'justifyContent': 'spaceAround',
+    'marginTop': '30px',
+    'marginLeft': '90px',
+    'marginRight': '90px'
+
+}
+
+graph_ex_style= {
+    'width':'10%',
+    'height': 'auto',
+    'backgroundColor': 'RGB(15,40,48)',
+    'layout': 'false'
+
+}
+
+sgraph_btn = {
+    'border': 'none',
+    'margin': '30px',
+    'background' : 'RGB(15,40,48)',
+    'borderWidth': '2px',
+    'borderColor':'black',
+    'borderStyle': 'solid',
+}
+
+chart_update_style={
+    'padding': '20px',
+    'display': 'flex',
+    'justifyContent': 'center',
+    'color': 'white'
+
+}
+
+next_btn_style ={
+    'backgroundColor': 'lightBlue',
+    'color': 'RGB(15,40,48)',
+    'borderColor': 'white',
+    'padding': '7px',
+    'borderRadius' : '6px',
+
+}
+
+show_data_style = {
+    'frontSize' : '17px',
+    'textAlign' : 'center',
+    'color' : 'white',
+}
+
+dropDownStyle = {
+    'background': 'RGB(15,40,48)',
+    # 'color': 'black',
+    'width': '330px',
+    'height': 'auto',
+}
+
+dropDownDivStyle = {
+    'background': 'RGB(15,40,48)',
+    'color': 'black',
+    'display': 'flex',
+    'justifyContent': 'center',
+    'marginTop': '30px',
+}
+
+xyzStyle = {
+    'marginLeft': '150px',
+    'fontSize': '20px',
+}
+
+btnStyle = {
+    'background' : 'RGB(15,40,48)',
+    'marginLeft' : '50%',
+    'marginTop' : '20px',
+}
+
+finalChartStyle = {
+    'display': 'flex',
+    'justifyContent':'center',
+    # 'borderColor': 'RGB(43,43,41)',
+    # 'borderWidth':'12px'
+}
